cda8ad6f472093a1f26072a3aec3757de658ded0
yakamok <38737288+yakamok@users.noreply.github.com>
2018-06-20 23:22:45 +0200

    Update gen-stats.py

--- gen-stats.py
+++ gen-stats.py
@@ -3,7 +3,7 @@ import json
 import datetime
 import requests
 
-ROOTDIR = "" #location you want the index.html file to be placed
+ROOTDIR = "/var/www/links.yakamo.org/test/" #location you want the index.html file to be placed
 CURRENTTIME = datetime.datetime.now()
 GETPEERS = '{"request": "getPeers","keepalive":true}'
 GETSESSIONS = '{"request": "getSessions","keepalive":true}'
@@ -38,7 +38,7 @@ def html_alpha(ipv6_self):
     aplha = '<!DOCTYPE html>\n \
     <html>\n \
     <head>\n \
-    <title>' + check_nodelist(NODELIST, ipv6_self) + '</title>\n \
+    <title>' + ipv6_self + '</title>\n \
     <link rel="stylesheet" type="text/css" href="style.css"/>\n \
     </head>\n \
     <body>\n \
@@ -68,54 +68,54 @@ def human_readable(bnum): # make bytes readable
 
 try:
     NODELIST = get_nodelist()
+    print "list exists"
 except:
+    print "failed"
     NODELIST = None
 
-try:
-    ygg = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ygg.connect(('localhost', 9001))
 
-    #fetch peer data
-    ygg.send(GETPEERS)
-    getPeers_data = json.loads(ygg.recv(1024 * 6))
+ygg = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ygg.connect(('localhost', 9001))
 
-    #fetch session data
-    ygg.send(GETSESSIONS)
-    getSessions_data = json.loads(ygg.recv(1024 * 6))
+#fetch peer data
+ygg.send(GETPEERS)
+getPeers_data = json.loads(ygg.recv(1024 * 6))
 
-    ygg.send(GETSELF)
-    this_node = json.loads(ygg.recv(1024 * 6))
+#fetch session data
+ygg.send(GETSESSIONS)
+getSessions_data = json.loads(ygg.recv(1024 * 6))
 
-    #write all stats into an html filled markdown file(doesnt really need to be a markdown file,
-    #just what my site pulls in by default)
-    with open(ROOTDIR + "index.html", "w") as handle:
-        handle.write(str(html_alpha(this_node['response']['self'].keys()[0])))
-        handle.write("<h3>Connected Peers</h3>\n  ")
+ygg.send(GETSELF)
+this_node = json.loads(ygg.recv(1024 * 6))
 
-        #write stats for connected peers here
-        for key, value in getPeers_data["response"]["peers"].iteritems():
-            if value['port'] != 0:
-                handle.write('<div class="peer">' + check_nodelist(NODELIST, key) + '</div>\n')
-                handle.write('<div class="data">')
-                handle.write('<div class="col1">Uptime: ' + str(datetime.timedelta(seconds=value['uptime'])) + '</div>\n')
-                handle.write('<div class="col2"> Rx: ' + human_readable(value['bytes_recvd']) + '</div>\n')
-                handle.write('<div class="col2"> Tx: ' + human_readable(value['bytes_sent']) + '</div>\n')
-                handle.write('<div class="col2"> Port: ' + str(value['port']) + '</div>\n')
-                handle.write('<div class="clear"></div>\n</div>\n')
+#write all stats into an html filled markdown file(doesnt really need to be a markdown file,
+#just what my site pulls in by default)
+with open(ROOTDIR + "index.html", "w") as handle:
+    handle.write(str(html_alpha(this_node['response']['self'].keys()[0])))
+    handle.write("<h3>Connected Peers</h3>\n  ")
 
-        handle.write("<h3>Current Sessions</h3>\n  ")
-        #write stats for current sessions
-        for key, value in getSessions_data["response"]["sessions"].iteritems():
+    #write stats for connected peers here
+    for key, value in getPeers_data["response"]["peers"].iteritems():
+        if value['port'] != 0:
             handle.write('<div class="peer">' + check_nodelist(NODELIST, key) + '</div>\n')
             handle.write('<div class="data">')
-            handle.write('<div class="col2">RX: ' + human_readable(value['bytes_recvd']) + '</div>\n')
-            handle.write('<div class="col2">TX: ' + human_readable(value['bytes_sent']) + '</div>\n')
-            handle.write('<div class="col2">Coords: ' + str(value['coords']) + '</div>\n')
-            handle.write('<div class="clear"></div>\n</div>')
+            handle.write('<div class="col1">Uptime: ' + str(datetime.timedelta(seconds=value['uptime'])) + '</div>\n')
+            handle.write('<div class="col2"> Rx: ' + human_readable(value['bytes_recvd']) + '</div>\n')
+            handle.write('<div class="col2"> Tx: ' + human_readable(value['bytes_sent']) + '</div>\n')
+            handle.write('<div class="col2"> Port: ' + str(value['port']) + '</div>\n')
+            handle.write('<div class="clear"></div>\n</div>\n')
 
-        #last updated time added here
-        handle.write('<div class="updated">Last updated: ' + str(CURRENTTIME) + '</div>\n')
-        handle.write('<a href="https://github.com/yakamok/yggdrasil-stats">ygg-stats</a>')
-        handle.write(HTMLOMEGA + "\n")
-except:
-    print "something went wrong"
+    handle.write("<h3>Current Sessions</h3>\n  ")
+    #write stats for current sessions
+    for key, value in getSessions_data["response"]["sessions"].iteritems():
+        handle.write('<div class="peer">' + check_nodelist(NODELIST, key) + '</div>\n')
+        handle.write('<div class="data">')
+        handle.write('<div class="col2">RX: ' + human_readable(value['bytes_recvd']) + '</div>\n')
+        handle.write('<div class="col2">TX: ' + human_readable(value['bytes_sent']) + '</div>\n')
+        handle.write('<div class="col2">Coords: ' + str(value['coords']) + '</div>\n')
+        handle.write('<div class="clear"></div>\n</div>')
+
+    #last updated time added here
+    handle.write('<div class="updated">Last updated: ' + str(CURRENTTIME) + '</div>\n')
+    handle.write('<a href="https://github.com/yakamok/yggdrasil-stats">ygg-stats</a>')
+    handle.write(HTMLOMEGA + "\n")
